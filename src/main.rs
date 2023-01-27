5358e52b3a0d7f38266fcf8f6b9e811f562acba6
priyaan99 <priyaanyadav@gmail.com>
2023-01-27 15:07:28 +0530

    working game

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::collections::LinkedList;
 use macroquad::prelude::*;
 
 const SIZE: usize = 25;
-const COLS: usize = 12;
-const ROWS: usize = 12;
+const COLS: usize = 15;
+const ROWS: usize = 15;
 const WIDHT: usize = SIZE * ROWS;
 const HEIGHT: usize = SIZE * COLS;
+
+const SNAKE_COLOR: Color = BLUE;
+const FOOD_COLOR: Color = RED;
 
 fn window_conf() -> Conf {
     Conf {
@@ -21,10 +24,13 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut out = false;
+    let mut score = 0;
     let mut snake = Snake::new();
+    let mut food_position = vec2(COLS as f32 / 2., ROWS as f32 / 2.).floor();
     let mut timer = 0.;
     let mut update_time = 0.;
-    let speed_time = 0.3;
+    const SPEED_TIME: f32 = 0.2;
+    let mut speed_time = SPEED_TIME;
 
     while !is_key_pressed(KeyCode::Escape) {
         if !out {
@@ -41,44 +47,103 @@ async fn main() {
             }
 
             if timer > update_time {
-                snake.update();
                 update_time += speed_time;
-            }
 
-            if snake.off_field(ROWS, COLS) {
-                out = true;
+                let random_position = || {
+                    vec2(
+                        rand::gen_range(0., ROWS as f32 - 1.),
+                        rand::gen_range(0., COLS as f32 - 1.),
+                    )
+                    .floor()
+                };
+
+                // snake vs food
+                if snake.head == food_position {
+                    score += 1;
+                    speed_time -= 0.005;
+                    snake.increase_len();
+
+                    while snake.contains_position(food_position) {
+                        food_position = random_position();
+                    }
+                }
+
+                snake.update(); // should be called afer increase len else collision to itself == true
+
+                if snake.off_field(ROWS, COLS) || snake.collided_itself() {
+                    out = true;
+                }
             }
         } else {
             // out
             if is_key_pressed(KeyCode::Enter) {
                 out = false;
+                score = 0;
                 snake = Snake::new();
+                food_position = vec2(COLS as f32 / 2., ROWS as f32 / 2.).floor();
+                timer = 0.;
+                update_time = 0.;
+                speed_time = SPEED_TIME;
             }
         }
 
-        clear_background(BLACK);
+        clear_background(WHITE);
 
         if !out {
+            // grid
             for r in 0..ROWS {
                 for c in 0..COLS {
-                    let color = if (r + c) % 2 == 0 { GRAY } else { BLACK };
-                    draw_rectangle(
+                    draw_rectangle_lines(
                         (r * SIZE) as f32,
                         (c * SIZE) as f32,
                         SIZE as f32,
                         SIZE as f32,
-                        color,
+                        3.,
+                        GRAY,
                     )
                 }
             }
 
+            // food
+            draw_rectangle(
+                food_position.x * SIZE as f32,
+                food_position.y * SIZE as f32,
+                SIZE as f32,
+                SIZE as f32,
+                FOOD_COLOR,
+            );
+
+            // snake
             snake.draw();
         } else {
             // out
+            const SCORE_FONT_SIZE: u16 = 35;
+            let score_str = &format!("SCORE : {}", score);
+            let score_measure = measure_text(score_str, None, FONT_SIZE, 1.);
+            draw_text(
+                score_str,
+                screen_width() / 2. - score_measure.width,
+                screen_height() * 1. / 3. - score_measure.height / 2.,
+                SCORE_FONT_SIZE as f32,
+                RED,
+            );
+
+            const FONT_SIZE: u16 = 20;
+            let restart_str = "Press Enter to Restart.";
+            let restart_measure = measure_text(restart_str, None, FONT_SIZE, 1.);
+            draw_text(
+                restart_str,
+                screen_width() / 2. - restart_measure.width / 2.,
+                screen_height() / 2. - restart_measure.height / 2.,
+                FONT_SIZE as f32,
+                RED,
+            );
         }
 
         next_frame().await
     }
+
+    println!("{}", score);
 }
 
 #[derive(PartialEq, Clone, Copy)]
@@ -120,6 +185,10 @@ impl Snake {
         }
     }
 
+    pub fn increase_len(&mut self) {
+        self.body.push_front(self.head);
+    }
+
     pub fn move_(&mut self, dir: Direction) {
         match dir {
             Direction::Up => {
@@ -152,7 +221,26 @@ impl Snake {
             || self.head.y >= cols as f32
     }
 
+    pub fn contains_position(&self, position: Vec2) -> bool {
+        self.head == position || self.body.contains(&position)
+    }
+
+    pub fn collided_itself(&self) -> bool {
+        self.body.contains(&self.head)
+    }
+
     pub fn update(&mut self) {
+        // self.body.push_front(self.head);
+        // self.body.pop_back();
+        // or
+
+        let mut prev_head = self.head;
+        for b in &mut self.body {
+            let t = *b;
+            *b = prev_head;
+            prev_head = t;
+        }
+
         self.head += self.direction.get_vec();
     }
 
@@ -162,7 +250,17 @@ impl Snake {
             self.head.y * SIZE as f32,
             SIZE as f32,
             SIZE as f32,
-            RED,
+            SNAKE_COLOR,
         );
+
+        for b in &self.body {
+            draw_rectangle(
+                b.x * SIZE as f32,
+                b.y * SIZE as f32,
+                SIZE as f32,
+                SIZE as f32,
+                SNAKE_COLOR,
+            );
+        }
     }
 }
